3857c2293171108651d89e5df97539bc0d4d3b4c
Alexei Lourie <alexeilourie@gmail.com>
2018-10-23 23:53:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,11 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr_names)
+  arr_messages = []
+  arr_names.each do |name|
+    arr_messages << "Hello, my name is #{name}."
+  end
 end
